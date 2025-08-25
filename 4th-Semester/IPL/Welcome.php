88a2a56646a2adaa6dd7f59c0752a7b16d08ca35
Omar Faruk Rakib <omarfarukrakib26@gmail.com>
2025-08-26 01:20:20 +0600

    Lab Report 1

---
+++ 4th-Semester/IPL/Welcome.php
@@ -0,0 +1,12 @@
+<?php
+
+$a = $_POST["email"];
+$b = $_POST["password"];
+$pass = "pass";
+
+if ($b != $pass) {
+    echo '<script> alert("wrong password")</script>';
+} else {
+    echo '<script> alert("Welcome!")</script>';
+    echo "Welcome to the Home page!";
+}
