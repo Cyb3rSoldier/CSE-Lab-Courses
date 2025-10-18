7b4da3106bffb0c54fcf48422647e1c81e81a751
Omar Faruk Rakib <omarfarukrakib26@gmail.com>
2025-10-18 23:31:37 +0600

    programming contest's problem

---
+++ codeforces/2^n bit.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    const long long MOD = 1e9 + 7;
+    long long n;
+    cin >> n;
+
+    long long base = 2, result = 1;
+    while (n > 0) {
+        if (n % 2 == 1)
+        {
+            result = (result * base) % MOD;
+        }
+        base = (base * base) % MOD;
+        n /= 2;
+    }
+
+    cout << result % MOD << endl;
+    return 0;
+}
