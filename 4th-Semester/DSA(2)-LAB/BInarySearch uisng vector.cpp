ecd5f923b472d4a8bd800883abd6d7acff3b1710
Omar Faruk Rakib <omarfarukrakib26@gmail.com>
2025-10-19 01:50:01 +0600

    2nd Week Updated

--- 4th-Semester/DSA(2)-LAB/BInarySearch uisng vector.cpp
+++ 4th-Semester/DSA(2)-LAB/BInarySearch uisng vector.cpp
@@ -1,42 +1,50 @@
-#include<iostream>
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
+
 int main()
 {
     vector<int> arr(100);
     int target;
-    int left, mid, right,totalE;
-    cout<<"Enter the size: ";
-    cin>>totalE;
-    cout<<"Total element in the array: ";
-    for(int index=0; index<totalE; index++){
-        cin>>arr[index];
+    int left, mid, right, totalE;
+
+    cout << "Enter the size: ";
+    cin >> totalE;
+
+    cout << "Total elements in the array: ";
+    for (int index = 0; index < totalE; index++) {
+        cin >> arr[index];
     }
-    /*
+
+          /*
     without arr size declaration
     for(int index=0; index<totalE; index++){
         cin>>x;
         arr.push_back(x);
     }*/
-    cout<<"Target is: ";
-    cin>>target;
-    left=0;
-    right= totalE-1;
-    while(left<=right){
 
-        mid = left+ (right-left)/2;
-        if(arr[mid]==target){
-            cout<<"Target index is: "<<mid<<endl;
+    sort(arr.begin(), arr.begin() + totalE);
+
+    cout << "Target is: ";
+    cin >> target;
+
+    left = 0;
+    right = totalE - 1;
+
+    while (left <= right)
+    {
+        mid = left + (right - left) / 2;
+
+        if (arr[mid] == target)
+        {
+            cout << "Target index is: " << mid << endl;
             return 0;
         }
-        else if(arr[left]>target){
-            right = mid-1;
-        }
-        else{
-            left = mid+1;
-        }
-
+        else if (arr[mid] > target)
+            right = mid - 1;
+        else
+            left = mid + 1;
     }
-    cout<<"Target Not Found!"<<endl;
+
+    cout << "Target Not Found!" << endl;
     return 0;
 }
--- codeforces/2^n bit.cpp
+++ codeforces/2^n bit.cpp
@@ -1,13 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+int main()
+{
     const long long MOD = 1e9 + 7;
     long long n;
     cin >> n;
 
     long long base = 2, result = 1;
-    while (n > 0) {
+    while (n > 0)
+    {
         if (n % 2 == 1)
         {
             result = (result * base) % MOD;
