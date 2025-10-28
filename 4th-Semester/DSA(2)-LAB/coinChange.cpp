ecfb237f33e7e86635e582b6c359ce01d4334f14
Omar Faruk Rakib <omarfarukrakib26@gmail.com>
2025-10-28 15:26:19 +0600

    8th week

--- 4th-Semester/DSA(2)-LAB/coinChange.cpp
+++ 4th-Semester/DSA(2)-LAB/coinChange.cpp
@@ -1,33 +1,35 @@
 #include<bits/stdc++.h>
-#define sz 500
+#define sz 1000
 using namespace std;
 
 int dp[11][sz];
 
 int main()
 {
-    int coin[] = {1,2,5,10,20,50,100,200,500,1000};
-    int amount = 15,n;
-    n= sizeof(coin)/sizeof(coin[0]);
+    int coin[]= {1,2,5,10,20,50,100,200,500,1000};
+    int amount = 775,i,j,n;
+    n=sizeof(coin)/sizeof(coin[0]);
     cout<<n<<endl;
-    for(int i =0; i<n; i++)
+    sort(coin,coin+n);
+    for(i=0; i<n; i++)
     {
-        for(int j =1; i<=amount; j++)
+        for(j=1; j<=amount; j++)
         {
-            if(j>=coin[i] && i==0)
-            {
-                dp[i][j] = 1+dp[i][j-coin[i]];
-            }
-            else if(j>=coin[i] && i>0){
-                dp[i][j]  = min(dp[i-1][j],1+dp[i][j-coin[i]]);
-            }
-            else if(i>0)
-            {
-                dp[i][j] = dp[i-1][j];
-            }
-
+            if(j>=coin[i]&&i==0)
+            dp[i][j]=1+dp[i][j-coin[i]];
+            else if(j>=coin[i]&&i>0)
+            dp[i][j]=min(dp[i-1][j],1+dp[i][j-coin[i]]);
+            else if(i>0) dp[i][j]=dp[i-1][j];
         }
     }
-    cout<<dp[n][amount];
-    return 0;
+    for(i=0; i<n; i++)
+    {
+        for(j=0; j<=amount; j++)
+        {
+            cout<<dp[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    cout<<dp[n-1][amount];
 }
+
