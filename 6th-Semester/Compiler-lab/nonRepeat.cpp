1ce4fa96c8e64187a0f281cd46555c15e7481398
Omar Faruk Rakib <omarfarukrakib26@gmail.com>
2026-08-07 01:55:19 +0600

    week 2

---
+++ 6th-Semester/Compiler-lab/nonRepeat.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    string s;
+    cout<<"Enter a String: ";
+    cin>>s;
+    for(int i = 0; i<s.size(); i++){
+        
+    }
+    return 0;
+}
---
+++ 6th-Semester/Compiler-lab/theMissingNumber.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter Limit:";
+    cin>>n;
+    int arr[n];
+    int sum = 0;
+    int sum1 = 0;
+
+    cout<<"Numbers: ";
+    for(int i = 0; i<n; i++){
+        cin>>arr[i];
+        sum = arr[i]+sum;
+    }
+
+    for(int i =1; i<=n; i++){
+        sum1 = i+sum1;
+    }
+
+    int res = sum1-sum;
+    cout<<res;
+
+
+    return 0;
+}
