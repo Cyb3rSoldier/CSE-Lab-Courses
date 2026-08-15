074cde78dedf34d127dc20fa5c0cc69cb6aafed9
Omar Faruk Rakib <omarfarukrakib26@gmail.com>
2026-08-16 01:05:15 +0600

    week 2

--- 6th-Semester/Computer-Graphics-and-Multimedia-Lab/draw a home/main.cpp
+++ 6th-Semester/Computer-Graphics-and-Multimedia-Lab/draw a home/main.cpp
@@ -15,61 +15,183 @@ void display()
 
     glPointSize(40);
     glBegin(GL_POINTS);
-        glColor3f(4.0,2.0,0.0);
-        glVertex2f(-0.7,0.7);
+    glColor3f(4.0,2.0,0.0);
+    glVertex2f(-0.7,0.7);
+    glEnd();
+
+    //========================
+    // roof ventilator
+    //========================
+
+    glBegin(GL_QUADS);
+    glColor3f(1.6,0.6,0.0);
+    glVertex2f(0.2,0.5);
+    glVertex2f(0.3,0.5);
+    glVertex2f(0.3,0.2);
+    glVertex2f(0.2,0.2);
     glEnd();
 
     //========================
     // Top of the home
     //========================
     glBegin(GL_TRIANGLES);
-        glColor3f(5.0,0.0,0.3);
-        glVertex2f(-0.4,0.2);
-        glVertex2f(0.4,0.2);
-        glVertex2f(0,0.4);
+    glColor3f(5.0,0.0,0.3);
+    glVertex2f(-0.4,0.2);
+    glVertex2f(0.4,0.2);
+    glVertex2f(0,0.4);
     glEnd();
 
     //========================
     // Body of the home
     //========================
     glBegin(GL_QUADS);
-        glColor3f(3.0,3.0,0.8);
-        glVertex2f(-0.3,-0.2);
-        glVertex2f(0.3,-0.2);
-        glVertex2f(0.3,0.2);
-        glVertex2f(-0.3,0.2);
+    glColor3f(3.0,3.0,0.8);
+    glVertex2f(-0.3,-0.2);
+    glVertex2f(0.3,-0.2);
+    glVertex2f(0.3,0.2);
+    glVertex2f(-0.3,0.2);
     glEnd();
 
+    //========================
+    // door
+    //========================
+
     glBegin(GL_QUADS);
-        glColor3f(1.6,0.6,0.0);
-        glVertex2f(0.2,0.5);
-        glVertex2f(0.3,0.5);
-        glVertex2f(0.3,0.3);
-        glVertex2f(0.2,0.3);
+    glColor3f(0.3,0.1,0.0);
+    glVertex2f(-0.1,-0.2);
+    glVertex2f(0.1,-0.2);
+    glVertex2f(0.1,0.1);
+    glVertex2f(-0.1,0.1);
     glEnd();
 
-    glBegin(GL_QUADS);
-        glColor3f(0.0,3.0,0.0);
-        glVertex2f(-0.1,-0.2);
-        glVertex2f(0.1,-0.2);
-        glVertex2f(0.1,0.1);
-        glVertex2f(-0.1,0.1);
-
-    glBegin(GL_QUADS);
-        glColor3f(0.0,3.0,3.0);
-        glVertex2f(-0.27,-0.01);
-        glVertex2f(-0.15,-0.01);
-        glVertex2f(-0.15,0.15);
-        glVertex2f(-0.27,0.15);
-
-    glBegin(GL_QUADS);
-        glColor3f(0.0,3.0,3.0);
-        glVertex2f(0.27,-0.01);
-        glVertex2f(0.15,-0.01);
-        glVertex2f(0.15,0.15);
-        glVertex2f(0.27,0.15);
-
+    //========================
+    // door line
+    //========================
+    glLineWidth(2);
+    glBegin(GL_LINES);
+    glColor3f(1.6,0.6,0.0);
+    glVertex2f(0,0.1);
+    glVertex2f(0,-0.2);
     glEnd();
+
+
+    //========================
+    // left window
+    //========================
+    glBegin(GL_QUADS);
+    glColor3f(1.0,0.2,0.1);
+    glVertex2f(-0.27,-0.01);
+    glVertex2f(-0.15,-0.01);
+    glVertex2f(-0.15,0.15);
+    glVertex2f(-0.27,0.15);
+    glEnd();
+
+    //========================
+    // left window lines
+    //========================
+
+    glLineWidth(1);
+    glBegin(GL_LINES);
+    glColor3f(1.6,0.6,0.0);
+    glVertex2f(-0.24,-0.01);
+    glVertex2f(-0.24,0.15);
+    glEnd();
+
+    glLineWidth(1);
+    glBegin(GL_LINES);
+    glColor3f(1.6,0.6,0.0);
+    glVertex2f(-0.21,-0.01);
+    glVertex2f(-0.21,0.15);
+    glEnd();
+
+    glLineWidth(1);
+    glBegin(GL_LINES);
+    glColor3f(1.6,0.6,0.0);
+    glVertex2f(-0.18,-0.01);
+    glVertex2f(-0.18,0.15);
+    glEnd();
+
+    glLineWidth(1);
+    glBegin(GL_LINES);
+    glColor3f(1.6,0.6,0.0);
+    glVertex2f(-0.27,0.03);
+    glVertex2f(-0.15,0.03);
+    glEnd();
+
+    glLineWidth(1);
+    glBegin(GL_LINES);
+    glColor3f(1.6,0.6,0.0);
+    glVertex2f(-0.27,0.07);
+    glVertex2f(-0.15,0.07);
+    glEnd();
+
+    glLineWidth(1);
+    glBegin(GL_LINES);
+    glColor3f(1.6,0.6,0.0);
+    glVertex2f(-0.27,0.11);
+    glVertex2f(-0.15,0.11);
+    glEnd();
+
+
+    //========================
+    // right window
+    //========================
+
+    glBegin(GL_QUADS);
+    glColor3f(1.0,0.2,0.1);
+    glVertex2f(0.27,-0.01);
+    glVertex2f(0.15,-0.01);
+    glVertex2f(0.15,0.15);
+    glVertex2f(0.27,0.15);
+    glEnd();
+
+    //========================
+    // right window lines
+    //========================
+
+    glLineWidth(1);
+    glBegin(GL_LINES);
+    glColor3f(1.6,0.6,0.0);
+    glVertex2f(0.24,-0.01);
+    glVertex2f(0.24,0.15);
+    glEnd();
+
+    glLineWidth(1);
+    glBegin(GL_LINES);
+    glColor3f(1.6,0.6,0.0);
+    glVertex2f(0.21,-0.01);
+    glVertex2f(0.21,0.15);
+    glEnd();
+
+    glLineWidth(1);
+    glBegin(GL_LINES);
+    glColor3f(1.6,0.6,0.0);
+    glVertex2f(0.18,-0.01);
+    glVertex2f(0.18,0.15);
+    glEnd();
+
+    glLineWidth(1);
+    glBegin(GL_LINES);
+    glColor3f(1.6,0.6,0.0);
+    glVertex2f(0.27,0.03);
+    glVertex2f(0.15,0.03);
+    glEnd();
+
+    glLineWidth(1);
+    glBegin(GL_LINES);
+    glColor3f(1.6,0.6,0.0);
+    glVertex2f(0.27,0.07);
+    glVertex2f(0.15,0.07);
+    glEnd();
+
+    glLineWidth(1);
+    glBegin(GL_LINES);
+    glColor3f(1.6,0.6,0.0);
+    glVertex2f(0.27,0.11);
+    glVertex2f(0.15,0.11);
+    glEnd();
+
+
 
     glFlush();
 }
@@ -91,7 +213,7 @@ int main(int argc,char** argv)
     glutInitWindowSize(800,700);
     glutInitWindowPosition(300,100);
 
-    glutCreateWindow("Lab 2 Work - Basic 2D Shapes");
+    glutCreateWindow("Sweet Home!");
 
     init();
 
