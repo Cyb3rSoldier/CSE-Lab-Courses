2d2d83affe04a5f05de75960e1ae3951a62e035f
Omar Faruk Rakib <omarfarukrakib26@gmail.com>
2026-09-06 01:15:12 +0600

    week 4

--- 6th-Semester/Microprocessors-Microcontrollers-and-Assembly-Language-Lab/caseConversion.asm
+++ 6th-Semester/Microprocessors-Microcontrollers-and-Assembly-Language-Lab/caseConversion.asm
@@ -1,34 +1,50 @@
 .model small
 .stack 100h
-.code
+
 .data
 msg1 db "Input: $"
 msg2 db 10,13,"Output: $"
 
+.code
 main proc
-    mov ax,@data
+    mov ax, @data
     mov ds, ax
-    
+
     mov ah, 09h
     lea dx, msg1
     int 21h
-    
+
     mov ah, 01h
-    int 21h  
-    mov bl,al
-    
+    int 21h
+    mov bl, al
+
+    cmp bl, 'a'
+    jb check_uppercase
+    cmp bl, 'z'
+    ja check_uppercase
+
     sub bl, 32
-    
+    jmp display_output
+
+check_uppercase:
+    cmp bl, 'A'
+    jb display_output
+    cmp bl, 'Z'
+    ja display_output
+
+    add bl, 32
+
+display_output:
     mov ah, 09h
     lea dx, msg2
     int 21h
-    
+
     mov ah, 02h
     mov dl, bl
     int 21h
-    
-    mov ah, 4ch
+
+    mov ah, 4Ch
     int 21h
-    
-    main endp
+
+main endp
 end main
